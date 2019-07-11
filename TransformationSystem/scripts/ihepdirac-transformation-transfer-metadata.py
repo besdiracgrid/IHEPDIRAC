470cbe41ba5cf19a4e7f2746020e22e748b23810
Xianghu Zhao <zhaoxh@ihep.ac.cn>
2019-07-11 14:42:33 +0800

    Use broadcast as default

--- TransformationSystem/scripts/ihepdirac-transformation-transfer-metadata.py
+++ TransformationSystem/scripts/ihepdirac-transformation-transfer-metadata.py
@@ -7,11 +7,15 @@ from DIRAC.Core.Base import Script
 
 Script.setUsageMessage('''Start transfer according to DFC query with transformation system
 
-{0} [option|cfgfile] TransformationName MetaTransfer SourceSE DestSE
+{0} [option|cfgfile] TransformationName MetaTransfer TargetSE
 
-Example: {0} Meassurements_DAQ_JINR -t Transfer-JUNO juno_transfer=PmtCharacterization/container_data/Meassurements_DAQ IHEP-STORM JINR-JUNO'''.format(Script.scriptName))
+Example: {0} Meassurements_DAQ_CNAF juno_transfer=Pmt/container/Meassurements CNAF-STORM
+Example: {0} Meassurements_DAQ_IN2P3 -p Standard juno_transfer=Pmt/container/Meassurements IN2P3-DCACHE
+Example: {0} Meassurements_DAQ_JINR -t Transfer-JUNO juno_transfer=Pmt/container/Meassurements -f IHEP-STORM JINR-JUNO'''.format(Script.scriptName))
 Script.registerSwitch( 't:', 'transformationType=', 'Specify transformation type')
 Script.registerSwitch( 'g:', 'groupSize=', 'Group size for each task')
+Script.registerSwitch( 'p:', 'plugin=', 'Plugin, default to "Broadcast"')
+Script.registerSwitch( 'f:', 'sourceSE=', 'SourceSE')
 Script.parseCommandLine(ignoreErrors = False)
 
 from DIRAC.DataManagementSystem.Client.MetaQuery import MetaQuery, FILE_STANDARD_METAKEYS
@@ -21,17 +25,18 @@ from DIRAC.TransformationSystem.Client.TransformationClient import Transformatio
 
 args = Script.getPositionalArgs()
 
-if len(args) != 4:
+if len(args) != 3:
     Script.showHelp()
     exit(1)
 
 transformationName = args[0]
 metaTransfer = args[1]
-fromSE = args[2]
-toSE = args[3]
+targetSE = args[2]
 
+sourceSE = ''
 groupSize = 100
 transformationType = 'Transfer-JUNO'
+plugin = 'Broadcast'
 
 switches = Script.getUnprocessedSwitches()
 for switch in switches:
@@ -39,6 +44,10 @@ for switch in switches:
         groupSize = int(switch[1])
     if switch[0] == 't' or switch[0] == 'transformationType':
         transformationType = switch[1]
+    if switch[0] == 'p' or switch[0] == 'plugin':
+        plugin = switch[1]
+    if switch[0] == 'f' or switch[0] == 'sourceSE':
+        sourceSE = switch[1]
 
 
 from DIRAC.Resources.Catalog.FileCatalogClient import FileCatalogClient
@@ -66,14 +75,24 @@ tc = TransformationClient( )
 t.setTransformationName(transformationName) # Must be unique
 t.setTransformationGroup("Transfer")
 t.setType(transformationType)
-#t.setPlugin("Standard") # Not needed. The default is 'Standard'
+t.setPlugin(plugin)
 t.setDescription("Data Transfer")
 t.setLongDescription("Data Transfer") # Mandatory
 t.setGroupSize(groupSize) # Here you specify how many files should be grouped within he same request, e.g. 100
 
-transBody = [ ( "ReplicateAndRegister", { "SourceSE": fromSE, "TargetSE": toSE }) ]
-
+transBody = ''
 t.setBody(transBody)
+
+if sourceSE:
+    res = t.setSourceSE(sourceSE)
+    if not res['OK']:
+        gLogger.error("SourceSE not valid: %s" % res['Message'])
+        exit(1)
+
+res = t.setTargetSE(targetSE)
+if not res['OK']:
+    gLogger.error("TargetSE not valid: %s" % res['Message'])
+    exit(1)
 
 result = t.addTransformation() # Transformation is created here
 if not result['OK']:
